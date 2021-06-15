7a51017b165219fdce3066bac921b12af4f2c812
mccleskeyc <mccleskeyc@gmail.com>
2021-06-15 12:32:21 -0400

    added useState hooks to manage state

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function SearchMovie() {
 
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+
     const searchMovies = async (e) => {
         e.preventDefault();
-        console.log("submitting");
-        const query = "Toy Story"
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            console.log(data);
+            console.log(data.results);
+            setMovies(data.results);
         }catch(err){
             console.error(err);
         }
@@ -20,7 +22,14 @@ export default function SearchMovie() {
     return (
         <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query" placeholder="ie Toy Story" autoComplete="off" />
+            <input 
+                className="input" 
+                type="text" 
+                name="query" 
+                placeholder="ie Toy Story" 
+                autoComplete="off"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)} />
             <button className="button" type="submit">Search</button>
         </form>
     )
