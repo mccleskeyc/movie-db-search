3641a7fd0658383a05df0ee8b5e6665dd9ca05d7
mccleskeyc <mccleskeyc@gmail.com>
2021-06-15 12:06:18 -0400

    added searchMovies function with fetch

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,10 +1,26 @@
 import React from "react";
 
 export default function SearchMovie() {
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitting");
+        const query = "Toy Story"
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+        
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            console.log(data);
+        }catch(err){
+            console.error(err);
+        }
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query" placeholder="ie Toy Story" />
+            <input className="input" type="text" name="query" placeholder="ie Toy Story" autoComplete="off" />
             <button className="button" type="submit">Search</button>
         </form>
     )
