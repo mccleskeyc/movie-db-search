e636e05132f6117824321043781193ff4308ded7
mccleskeyc <mccleskeyc@gmail.com>
2021-06-23 16:07:42 -0400

    created links to tmdb pages for each suggestion

--- src/components/SuggestionCard.js
+++ src/components/SuggestionCard.js
@@ -5,10 +5,12 @@ export default function SuggestionCard({movie}){
 
         return (
             <>
+               <a href= {`https://www.themoviedb.org/movie/${movie.id}`}>
                <img className="card-image-mini"
                    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
                    alt={movie.title + ' poster'}
                    />
+                   </a>
            </>
            
        )
